3e9717bc0e05b5dea7cede458b6f7436d9d35fa3
Tanishq-Rajpuria <157007512+tanishqrajpuria@users.noreply.github.com>
2025-06-16 15:53:31 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+
+/**
+ * navbar toggle
+ */
+
+const header = document.querySelector("[data-header]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+
+navToggleBtn.addEventListener("click", function () {
+  header.classList.toggle("nav-active");
+  this.classList.toggle("active");
+});
+
+/**
+ * toggle the navbar when click any navbar link
+ */
+
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    header.classList.toggle("nav-active");
+    navToggleBtn.classList.toggle("active");
+  });
+}
+
+
+
+/**
+ * back to top & header
+ */
+
+const backTopBtn = document.querySelector("[data-back-to-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  emailjs.sendForm("service_t2qnaup", "template_s29y5wn", this)
+    .then(function(response) {
+       alert("Message sent successfully!");
+       document.getElementById("contact-form").reset();
+    }, function(error) {
+       alert("Failed to send message. Try again.");
+       console.log("FAILED...", error);
+    });
+});
